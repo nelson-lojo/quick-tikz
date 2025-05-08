5ea1db82cf0375f7c8fbed7cabd2a0f62396e1ff
Chris Ikeokwu <christianthrone@gmail.com>
2025-05-08 15:27:57 -0700

    changed export to clipboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -142,19 +142,32 @@ export default function Home() {
     // Function to export the code as a .tex file
     // This function creates a Blob from the code and triggers a download
     // adds the tikzpicture environment
+    // function exportCode(code: string | undefined) {
+    //     if (code) {
+    //         const tikzCode = `\\begin{tikzpicture}\n${code}\n\\end{tikzpicture}`;
+    //         const blob = new Blob([tikzCode], {type: 'text/plain'});
+    //         const url = URL.createObjectURL(blob);
+    //         const a = document.createElement('a');
+    //         a.href = url;
+    //         a.download = 'quick_tikz_code.tex';
+    //         a.click();
+    //         URL.revokeObjectURL(url);
+    //     }
+    // }
+
+      // function to export the code to the clipboard
     function exportCode(code: string | undefined) {
         if (code) {
             const tikzCode = `\\begin{tikzpicture}\n${code}\n\\end{tikzpicture}`;
-            const blob = new Blob([tikzCode], {type: 'text/plain'});
-            const url = URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'quick_tikz_code.tex';
-            a.click();
-            URL.revokeObjectURL(url);
+            navigator.clipboard.writeText(tikzCode)
+                .then(() => {
+                    console.log('Code copied to clipboard');
+                })
+                .catch(err => {
+                    console.error('Failed to copy code: ', err);
+                });
         }
     }
-
     console.log("rendering Home");
 
     return (
